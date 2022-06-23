a289157f7199e35c4ae57a1f9db88b99f3617fe0
OLGA <serikova-olga@mail.ru>
2022-06-23 16:53:54 +0300

    task15

---
+++ ex15/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.WriteLine ("input number of week day 1-7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<6)
+{
+    Console.WriteLine("no");
+}
+else
+{
+    Console.WriteLine("yes");
+}
